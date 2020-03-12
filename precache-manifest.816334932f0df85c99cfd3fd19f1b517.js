ce7de9181e5a746ac3f2aa6b7368c6071e9fd67a
yue <2337610467@qq.com>
2020-03-12 19:40:33 +0800

    tijiao

---
+++ precache-manifest.816334932f0df85c99cfd3fd19f1b517.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5670a9ac5a92c78be07bb3c40f1b22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5603368ebff6ba6e7c94",
+    "url": "/static/css/2.75397b69.chunk.css"
+  },
+  {
+    "revision": "5265c716ccb92ebf7cd9",
+    "url": "/static/css/main.9cdfdfa9.chunk.css"
+  },
+  {
+    "revision": "5603368ebff6ba6e7c94",
+    "url": "/static/js/2.251b2509.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.251b2509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5265c716ccb92ebf7cd9",
+    "url": "/static/js/main.060e0b92.chunk.js"
+  },
+  {
+    "revision": "6404d727a67f55bd007d",
+    "url": "/static/js/runtime-main.4d3ca6e5.js"
+  },
+  {
+    "revision": "c305c06e69ce9911d10bbde0236f971e",
+    "url": "/static/media/logo.c305c06e.png"
+  }
+]);
